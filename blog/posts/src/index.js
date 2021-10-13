fcc78bba786d97ef5eb22c429eebcc0562513238
Thiago Galvani <thiagopaiva99@gmail.com>
2021-10-13 20:01:10 -0300

    creating the first version for get and store posts on post service

---
+++ blog/posts/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const { randomBytes } = require('crypto');
+
+const posts = {};
+app.use(bodyParser.json());
+
+app.get('/posts', (req, res) => {
+  res.send(Object.values(posts));
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+  posts[id] = { id, title };
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Listening on port 4000');
+});
