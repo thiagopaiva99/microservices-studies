2e96ded122f1376676d593b3438d11770f572f8c
Thiago Galvani <thiagopaiva99@gmail.com>
2021-10-13 20:27:32 -0300

    adding cors for posts and comments service

--- blog/posts/src/index.js
+++ blog/posts/src/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
-app.use(bodyParser.json());
 
 app.get('/posts', (req, res) => {
   res.send(Object.values(posts));
