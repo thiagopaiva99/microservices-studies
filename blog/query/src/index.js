abaec82384769281368ddbde4e67865d4fe2e3ae
Thiago Galvani <thiagopaiva99@gmail.com>
2021-10-14 19:37:14 -0300

    creating the first version of query service

---
+++ blog/query/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/posts', (req, res) => {});
+
+app.post('/events', (req, res) => {});
+
+app.listen(4002, () => {
+  console.log(`[Query Service] Listening on port 4002`);
+});
