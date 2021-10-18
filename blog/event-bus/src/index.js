ff049d907ec15892ed198deef2e424a45864217f
Thiago Galvani <thiagopaiva99@gmail.com>
2021-10-18 18:56:42 -0300

    creating an endpoint for list all events on event bus

--- blog/event-bus/src/index.js
+++ blog/event-bus/src/index.js
@@ -6,8 +6,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post('http://localhost:4000/events', event).catch(console.log);
   axios.post('http://localhost:4001/events', event).catch(console.log);
@@ -15,6 +19,10 @@ app.post('/events', (req, res) => {
   axios.post('http://localhost:4003/events', event).catch(console.log);
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
