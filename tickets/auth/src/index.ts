100cc1437f5246e21875e7158fcfc3a9e05ed0a0
Thiago Galvani <thiagopaiva99@gmail.com>
2021-10-25 20:17:35 -0300

    creating ingress file for auth service

--- tickets/auth/src/index.ts
+++ tickets/auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(json());
 
+app.get('/api/users/current-user', (req, res) => {
+    return res.send('Hi there')
+})
+
 app.listen(3000, () => {
     console.log('[Auth Service] Listening on port 3000');
 })
