a9c000f0e80b9b925261e211f004f05499834326
Thiago Galvani <thiagopaiva99@gmail.com>
2021-10-25 20:35:26 -0300

    creating router for current user

--- tickets/auth/src/routes/current-user.ts
+++ tickets/auth/src/routes/current-user.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/current-user', () => {
-
+router.get('/api/users/current-user', (req, res) => {
+    return res.send('Hi there (current user');
 })
 
 export { router as currentUserRouter };
