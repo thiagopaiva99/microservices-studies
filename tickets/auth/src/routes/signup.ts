c468d0f4db232e3e49e03ac7c7a7fb8fe44ad4b5
Thiago Galvani <thiagopaiva99@gmail.com>
2021-10-26 19:38:34 -0300

    creating signup body validator with express validators

--- tickets/auth/src/routes/signup.ts
+++ tickets/auth/src/routes/signup.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-    return res.send('Hi there (signup');
+const signupBodyValidator = [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Password must be between 4 and 20 characters'),
+]
+
+router.post('/api/users/signup', signupBodyValidator, (req: Request, res: Response) => {
+    const { email, password } = req.body;    
 })
 
 export { router as signupRouter };
